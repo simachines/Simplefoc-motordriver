1c619f7f066fad608e34dc994cf276f3b117bcfb
SiMachines <ernest18699@gmail.com>
2026-02-22 05:34:37 -0500

    enhance main.cpp with improved ADC handling and brake control logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "SimpleFOC.h"
-//#include "SimpleFOCDrivers.h"
 #include "encoders/stm32hwencoder/STM32HWEncoder.h"
 #include "encoders/mt6835/MagneticSensorMT6835.h"
 #include "drivers/hardware_specific/stm32/stm32_mcu.h"
@@ -15,6 +14,7 @@
 #if defined(STM32G4)
 #include "utilities/stm32math/STM32G4CORDICTrigFunctions.h"
 #endif
+//#define monitoring_enabled
 
 #define CLAMP(x, low, high) (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
 #if defined(STM32G4)
@@ -42,8 +42,11 @@
 #define MT6835_SPI_CS   PA15
 uint16_t BRAKE_RESISTANCE = 5 * 100;   // Ohms * 100
 constexpr int pole_pairs = 15;
-float phase_resistance = 0.3f; // Ohms
 constexpr int supply_voltage_V = 24;
+constexpr float ADC_REF_V = 3.3f;
+float phase_resistance = 0.3f; // Ohms
+float maxCurrent = 10;
+float alignStrength = 4;
 #if defined(PWM_INPUT)
 STM32PWMInput pwmInput = STM32PWMInput(PA3);
 #endif
@@ -66,8 +69,8 @@ static void configureBtsBreak(void);
 #define BTS_OC_ACTIVE_LOW false
 #define FAULT_LED_PIN LED_BUILTIN
 #define A_VBUS PA1
-#define currentPHA _NC
-#define currentPHB PA2
+#define currentPHA PA2
+#define currentPHB _NC
 #define currentPHC PA3
 #define ENCODER_PIN_A PC6
 #define ENCODER_PIN_B PB5
@@ -79,10 +82,12 @@ uint16_t BRAKE_RESISTANCE = 2 * 100;   // Ohms * 100
 float phase_resistance = 5.0f; // Ohms
 constexpr int pole_pairs = 6;
 constexpr int supply_voltage_V = 24;
+constexpr float ADC_REF_V = 3.0f;
+float maxCurrent = 4;
+float alignStrength = 10;
 #if defined(PWM_INPUT)
 STM32PWMInput pwmInput = STM32PWMInput(PE5);
 #endif
-
 #endif
 
 
@@ -96,8 +101,6 @@ STM32PWMInput pwmInput = STM32PWMInput(PE5);
 float degrees = 0;
 float phase_inductance = 0.0003;
 float motor_KV = _NC;
-float maxCurrent = 10;
-float alignStrength = 4;
 float current_bandwidth = 100; //hz
 float a, b, c;
 uint16_t pwmPeriodCounts = 0;
@@ -128,6 +131,7 @@ uint32_t main_loop_counter_prev = 0;
 uint32_t control_loop_counter_prev = 0;
 uint16_t MAX_REGEN_CURRENT = 0 * 100; // Amps * 100
 uint16_t BRKRESACT_SENS = 1;     // Threshold in Amps x 100
+int16_t regenCur;
 float v_bus = 0.00f;
 
 #if defined(BRAKE_VOLTAGE_RAMP_ENABLED)
@@ -137,17 +141,20 @@ constexpr float BRAKE_OVERVOLTAGE_RAMP_END_V = supply_voltage_V + 1.5f;
 constexpr float VBUS_RESISTOR_TOP_OHMS = 10000.0f;
 constexpr float VBUS_RESISTOR_BOTTOM_OHMS = 1000.0f;
 constexpr float v_bus_scale = (VBUS_RESISTOR_TOP_OHMS + VBUS_RESISTOR_BOTTOM_OHMS) / VBUS_RESISTOR_BOTTOM_OHMS; // divider ratio
-constexpr float ADC_REF_V = 3.3f;
 constexpr float ADC_MAX_COUNTS = 4095.0f;
-constexpr float VBUS_ADC_SCALE = ADC_REF_V * ADC_MAX_COUNTS;
+constexpr float VBUS_ADC_SCALE = ADC_REF_V / ADC_MAX_COUNTS;
+static ADC_HandleTypeDef hadc2 = {0};
+static DMA_HandleTypeDef hdma_adc2 = {0};
+static volatile uint32_t vbus_adc2_dma_raw = 0;
+static bool vbus_adc2_ready = false;
 // Motor and driver objects
 SimpleFOCDebug debug;
 BLDCMotor motor = BLDCMotor(pole_pairs, phase_resistance, motor_KV, phase_inductance);
 BLDCDriver3PWM driver = BLDCDriver3PWM((int)PH_A, (int)PH_B, (int)PH_C, (int)BTS_ENABLE);
 #if defined(STM32F4)
-LowsideCurrentSense current_sense = LowsideCurrentSense(0.035, 50.0f, currentPHA, currentPHB, currentPHC);
+LowsideCurrentSense currentsense = LowsideCurrentSense(0.035, 50.0f, currentPHA, currentPHB, currentPHC);
 #elif defined(STM32G4)
-LowsideCurrentSense current_sense = LowsideCurrentSense(66.0mV , currentPHA, currentPHB, currentPHC);
+LowsideCurrentSense currentsense = LowsideCurrentSense(0.066f, currentPHA, currentPHB, currentPHC);
 #endif
 STM32HWEncoder encoder = STM32HWEncoder(ENCODER_PPR, ENCODER_PIN_A, ENCODER_PIN_B, _NC);
 SPIClass SPI_3((int)MT6835_SPI_MOSI, (int)MT6835_SPI_MISO, (int)MT6835_SPI_SCK);
@@ -157,10 +164,12 @@ Commander commander = Commander(Serial);
 void calc_hw_pwm();
 void check_vbus();
 #if defined(BRAKE_CONTROL_ENABLED)
- void vbus_sense_adc_cb(uint32_t adc_value);
  void brake_control(void);
  static bool configureBrakePwm(void);
 #endif
+static bool init_vbus_adc2_dma(void);
+static inline float vbus_from_dma_counts(void);
+static inline volatile uint32_t* vbus_adc2_dma_address(void);
 void onMotor(char* cmd){ commander.motor(&motor,cmd); }
 
 static inline uint32_t get_systick_time_us() {
@@ -177,6 +186,134 @@ static inline uint32_t get_systick_time_us() {
 
   const uint32_t sub_ms_us = ((reload - ticks) * 1000u) / reload;
   return (ms_start * 1000u) + sub_ms_us;
+}
+
+static bool init_vbus_adc2_dma(void) {
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+#if defined(STM32F4)
+  __HAL_RCC_ADC2_CLK_ENABLE();
+  __HAL_RCC_DMA2_CLK_ENABLE();
+#elif defined(STM32G4)
+  __HAL_RCC_ADC12_CLK_ENABLE();
+  __HAL_RCC_DMA1_CLK_ENABLE();
+#ifdef __HAL_RCC_DMAMUX1_CLK_ENABLE
+  __HAL_RCC_DMAMUX1_CLK_ENABLE();
+#endif
+#else
+  return false;
+#endif
+
+  GPIO_InitTypeDef gpio = {0};
+  gpio.Pin = GPIO_PIN_0;
+#if defined(STM32F4)
+  gpio.Pin = GPIO_PIN_1;
+#endif
+  gpio.Mode = GPIO_MODE_ANALOG;
+  gpio.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &gpio);
+
+  hadc2.Instance = ADC2;
+#if defined(STM32F4)
+  hadc2.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc2.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc2.Init.ScanConvMode = DISABLE;
+  hadc2.Init.ContinuousConvMode = ENABLE;
+  hadc2.Init.DiscontinuousConvMode = DISABLE;
+  hadc2.Init.NbrOfDiscConversion = 0;
+  hadc2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc2.Init.NbrOfConversion = 1;
+  hadc2.Init.DMAContinuousRequests = ENABLE;
+  hadc2.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+#elif defined(STM32G4)
+  hadc2.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc2.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc2.Init.GainCompensation = 0;
+  hadc2.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc2.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc2.Init.LowPowerAutoWait = DISABLE;
+  hadc2.Init.ContinuousConvMode = ENABLE;
+  hadc2.Init.NbrOfConversion = 1;
+  hadc2.Init.DiscontinuousConvMode = DISABLE;
+  hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc2.Init.DMAContinuousRequests = ENABLE;
+  hadc2.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+  hadc2.Init.OversamplingMode = DISABLE;
+#endif
+
+#if defined(STM32F4)
+  hdma_adc2.Instance = DMA2_Stream2;
+  hdma_adc2.Init.Channel = DMA_CHANNEL_1;
+  hdma_adc2.Init.Direction = DMA_PERIPH_TO_MEMORY;
+  hdma_adc2.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_adc2.Init.MemInc = DMA_MINC_DISABLE;
+  hdma_adc2.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+  hdma_adc2.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+  hdma_adc2.Init.Mode = DMA_CIRCULAR;
+  hdma_adc2.Init.Priority = DMA_PRIORITY_HIGH;
+  hdma_adc2.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+#elif defined(STM32G4)
+  hdma_adc2.Instance = DMA1_Channel1;
+  hdma_adc2.Init.Request = DMA_REQUEST_ADC2;
+  hdma_adc2.Init.Direction = DMA_PERIPH_TO_MEMORY;
+  hdma_adc2.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_adc2.Init.MemInc = DMA_MINC_DISABLE;
+  hdma_adc2.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+  hdma_adc2.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+  hdma_adc2.Init.Mode = DMA_CIRCULAR;
+  hdma_adc2.Init.Priority = DMA_PRIORITY_HIGH;
+#endif
+
+  if (HAL_DMA_Init(&hdma_adc2) != HAL_OK) {
+    return false;
+  }
+  __HAL_LINKDMA(&hadc2, DMA_Handle, hdma_adc2);
+
+  if (HAL_ADC_Init(&hadc2) != HAL_OK) {
+    return false;
+  }
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+  sConfig.Channel = ADC_CHANNEL_1;
+#if defined(STM32G4)
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_640CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+#elif defined(STM32F4)
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+#endif
+
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK) {
+    return false;
+  }
+
+#if defined(STM32G4)
+  if (HAL_ADCEx_Calibration_Start(&hadc2, ADC_SINGLE_ENDED) != HAL_OK) {
+    return false;
+  }
+#endif
+
+  if (HAL_ADC_Start_DMA(&hadc2, const_cast<uint32_t*>(&vbus_adc2_dma_raw), 1) != HAL_OK) {
+    return false;
+  }
+
+  return true;
+}
+
+static inline float vbus_from_dma_counts(void) {
+  const float pin_voltage = static_cast<float>(vbus_adc2_dma_raw & 0x0FFFu) * VBUS_ADC_SCALE;
+  return pin_voltage * v_bus_scale;
+}
+
+static inline volatile uint32_t* vbus_adc2_dma_address(void) {
+  return &vbus_adc2_dma_raw;
 }
 
 void setBandwidth(char* cmd) {
@@ -315,15 +452,20 @@ void setup(){
     commander.add('B', setBandwidth, "Set current control bandwidth (Hz)");
     commander.add('M', onMotor, "my motor motion");
 	pinMode(FAULT_LED_PIN, OUTPUT);
-  pinMode(A_VBUS, INPUT_ANALOG);
-	
+  vbus_adc2_ready = init_vbus_adc2_dma();
+  if (!vbus_adc2_ready) {
+    Serial.println("VBUS ADC2 init failed");
+  } else {
+    Serial.printf("VBUS ADC2 DMA addr: 0x%08lX\n", (uint32_t)vbus_adc2_dma_address());
+  }
+	pwmInput.initialize();
   #if defined(PWM_INPUT)
    if (pwmInput.initialize() != 0) {
       Serial.println("PWM input init failed");
     }
     #endif
-  //current_sense.gain_a *= -1;
-  //current_sense.gain_c *= -1;
+  currentsense.gain_a *= -1;
+  currentsense.gain_c *= -1;
   driver.voltage_power_supply = supply_voltage_V;  // Convert mV to V for driver
   driver.voltage_limit = driver.voltage_power_supply*0.9;
   driver.pwm_frequency = PWM_FREQ;
@@ -353,11 +495,11 @@ void setup(){
 
   motor.linkSensor(&encoder); 
   motor.linkDriver(&driver); 
-  motor.linkCurrentSense(&current_sense);
-  current_sense.linkDriver(&driver);
-  int cs_init = current_sense.init();
-  Serial.printf("Current sense init status: %d\n", cs_init);
-  v_bus = analogRead(A_VBUS) * v_bus_scale / 100;
+  motor.linkCurrentSense(&currentsense);
+  currentsense.linkDriver(&driver);
+
+  
+  v_bus = vbus_adc2_ready ? vbus_from_dma_counts() : 0.0f;
 	 while (v_bus < supply_voltage_V - 1.0f  || v_bus > supply_voltage_V + 1.0f) {
     //while (v_bus > supply_voltage_V - 1.0f) {
     digitalWrite(FAULT_LED_PIN, HIGH);
@@ -365,11 +507,16 @@ void setup(){
     delay(250); // Small delay to avoid busy-waiting
     digitalWrite(FAULT_LED_PIN, LOW);
     delay(250);
-    v_bus = analogRead(A_VBUS) * v_bus_scale / 100;
+    v_bus = vbus_adc2_ready ? vbus_from_dma_counts() : 0.0f;
   }
   digitalWrite(FAULT_LED_PIN, HIGH);
   Serial.printf("PSU NOMINAL: %.2f V\n", v_bus);
-  motor.useMonitoring(Serial);  
+   
+    int cs_init = currentsense.init();
+  Serial.printf("Current sense init status: %d\n", cs_init);
+  #if (monitoring_enabled)
+  motor.useMonitoring(Serial); 
+  #endif 
   //motor.controller = MotionControlType::torque;
   motor.controller = MotionControlType::torque;
   motor.torque_controller = TorqueControlType::foc_current;
@@ -400,7 +547,7 @@ void setup(){
  
   int m_init = motor.init();
   Serial.printf("Motor init status: %d\n", m_init);
-  //current_sense.skip_align = true; // before initFOC()
+  //currentsense.skip_align = true; // before initFOC()
 
   int foc_init = motor.initFOC();
   Serial.printf("FOC init status: %d\n", foc_init);
@@ -414,12 +561,14 @@ void loop() {
 	 motor.loopFOC();
      commander.run();
      motor.move();
-     
+     check_vbus();
   const uint32_t now_us = get_systick_time_us();
   if ((uint32_t)(now_us - t_control_us) >= CONTROL_LOOP_PERIOD_US) {
     t_control_us += CONTROL_LOOP_PERIOD_US;
     control_loop_counter++;
+    #if (monitoring_enabled)
     motor.monitor();
+    #endif
   #if defined(PWM_INPUT)
     calc_hw_pwm();
   #endif
@@ -431,7 +580,7 @@ void loop() {
     brake_control();
     #endif
   #endif
-    check_vbus();
+    
     
     degrees = encoder.getMechanicalAngle() * RAD_2_DEG;
   }
@@ -483,11 +632,13 @@ void calc_hw_pwm(void){
 #endif
 
 void check_vbus() {
-  //v_bus = _readADCVoltageLowSide(A_VBUS,current_sense.params)*v_bus_scale;
-    //a = _readADCVoltageLowSide(currentPHA,current_sense.params);
-   // b = _readADCVoltageLowSide(currentPHB,current_sense.params);
-   // c = _readADCVoltageLowSide(currentPHC,current_sense.params);
-  //
+  if (vbus_adc2_ready) {
+    v_bus = vbus_from_dma_counts();
+  }
+  else {
+    return;
+  }
+
   //driver.voltage_power_supply = v_bus;
   //driver.voltage_limit = driver.voltage_power_supply*0.9;
   //motor.voltage_limit = driver.voltage_power_supply *0.58f;
@@ -511,21 +662,29 @@ void check_vbus() {
 #if defined(BRAKE_CONTROL_ENABLED)
 
  void brake_control(void){
-  static int16_t regenCur = -current_sense.getDCCurrent(motor.electrical_angle) * 100 - MAX_REGEN_CURRENT;
+    regenCur = -currentsense.getDCCurrent(motor.electrical_angle) * 100 - MAX_REGEN_CURRENT;
+    static float appliedBrakeDuty = 0.0f;
     const int16_t brkOnThresh = BRKRESACT_SENS;
     const int16_t brkOffThresh = (BRKRESACT_SENS > 1) ? (BRKRESACT_SENS / 2) : 0;
     float brakeDuty = 0.0f;
+    constexpr float BRAKE_DUTY_RAMP_DOWN_STEP = 0.003f;
+
+#if defined(BRAKE_VOLTAGE_RAMP_ENABLED)
+    const bool overvoltageRequest = (v_bus > BRAKE_OVERVOLTAGE_RAMP_START_V);
+#else
+    const bool overvoltageRequest = false;
+#endif
 
     if (regenCur < 0) {
       regenCur = 0;
     }
 
     if (!brake_active) {
-      if (regenCur > brkOnThresh) {
+      if ((regenCur > brkOnThresh) || overvoltageRequest) {
         brake_active = 1;
       }
     } else {
-      if (regenCur < brkOffThresh) {
+      if ((regenCur < brkOffThresh) && !overvoltageRequest) {
         brake_active = 0;
       }
     }
@@ -537,18 +696,29 @@ void check_vbus() {
       brakeDuty = ((float)regenCur * (float)BRAKE_RESISTANCE) / (vbus_for_duty * 10000.0f);
 
 #if defined(BRAKE_VOLTAGE_RAMP_ENABLED)
-      if ((BRAKE_OVERVOLTAGE_RAMP_END_V > BRAKE_OVERVOLTAGE_RAMP_START_V) && (v_bus > BRAKE_OVERVOLTAGE_RAMP_START_V)) {
+      if ( v_bus > BRAKE_OVERVOLTAGE_RAMP_START_V) {
         const float rampSpan = BRAKE_OVERVOLTAGE_RAMP_END_V - BRAKE_OVERVOLTAGE_RAMP_START_V;
         brakeDuty += (v_bus - BRAKE_OVERVOLTAGE_RAMP_START_V) / rampSpan;
       }
 #endif
-
-      brakeDuty = CLAMP(brakeDuty, 0.0f, 0.90f);
-      I_Bus = (int16_t)(brakeDuty * pwmPeriodCounts);
-      __HAL_TIM_SET_COMPARE(&htim_brake, BRAKE_PWM_CHANNEL, I_Bus);
-    } else {
-      __HAL_TIM_SET_COMPARE(&htim_brake, BRAKE_PWM_CHANNEL, 0);
     }
+
+    brakeDuty = CLAMP(brakeDuty, 0.0f, 0.90f);
+
+    if (brakeDuty < appliedBrakeDuty) {
+      appliedBrakeDuty -= BRAKE_DUTY_RAMP_DOWN_STEP;
+      if (appliedBrakeDuty < brakeDuty) {
+        appliedBrakeDuty = brakeDuty;
+      }
+      if (appliedBrakeDuty < 0.0f) {
+        appliedBrakeDuty = 0.0f;
+      }
+    } else {
+      appliedBrakeDuty = brakeDuty;
+    }
+
+    I_Bus = (int16_t)(appliedBrakeDuty * pwmPeriodCounts);
+    __HAL_TIM_SET_COMPARE(&htim_brake, BRAKE_PWM_CHANNEL, I_Bus);
  }
 #endif
 
